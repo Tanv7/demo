4cbbc85f0c68f2b568b7a6e0504bf70690c81868
Tanv7 <tanv7@yandex.ru>
2024-04-24 16:46:23 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/SpringTask1Application.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+@SpringBootApplication
+    public class SpringTask1Application {
+
+        public static void main(String[] args) {
+            SpringApplication.run(SpringTask1Application.class, args);
+        }
+
+    }
+
